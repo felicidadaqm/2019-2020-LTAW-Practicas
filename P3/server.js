1b7184ab4ab0726ddc1d1d8e819cc46b9e50b7bd
felicidadaqm <felicidadaqm@gmail.com>
2020-04-15 20:15:53 +0200

    Añadido carrito y precio final de la compra.

--- P3/server.js
+++ P3/server.js
@@ -22,7 +22,7 @@ function recortar(datos, caracter) {
 
 // Creamos un html muy básico para las páginas de confirmación
 // -- ARREGLAR PARA QUE SIRVA PARA LOS CARRITOS
-function htmlbase(tipoconf, variable) {
+function htmlbase(tipoconf, variable, boton) {
   context =  `
     <!DOCTYPE html>
     <html lang="es">
@@ -41,20 +41,25 @@ function htmlbase(tipoconf, variable) {
         <div class="main"> `
     context += tipoconf
     context += `
-          <div class="cuadro">
-          <p>¡Bienvenido `
+          <div class="cuadro"> `
     context += variable
     context += `
-          !</p>
           </div>
           <br>
-          <br>
-          <a href="/" class="button">Pagina principal</a>
+          <br> `
+    context += boton
+    context += `
         </div>
       </body>
       </html>
       `
     return context
+}
+
+function constructor(req, res, content, mime) {
+  res.setHeader('Content-Type', mime)
+  res.write(content);
+  res.end();
 }
 
 //-- Funcion para atender a una Peticion
@@ -101,18 +106,19 @@ function peticion(req, res) {
     case "/":
       mime = "text/html"
       // -- Primero comprobamos que el usuario esté loggeado.
-      //-- No hay ninguna cookie
       if (!cookie) {
         filename += "nologgeado.html"
       //-- Hay definida una Cookie. Entra a la vista principal
       } else {
         filename += "index.html"
       }
+
+      content = fs.readFileSync(filename, "utf-8")
+      constructor(req, res, content, mime)
       break;
 
     //-- Pagina de registro
     case "/login":
-      // ARREGLAR ESTO
       mime = "text/html"
       filename = "login.html"
 
@@ -124,7 +130,9 @@ function peticion(req, res) {
               //-- Añadimos el usuario al html base
               usuario= recortar(data, "=")
               confirmacion = "<h2>Confirmación de registro</h2>"
-              content = htmlbase(confirmacion, usuario)
+              usuario1 = "<p>¡Bienvenido " + usuario + "!</p>"
+              boton =  '<a href="/" class="button">Pagina principal</a>'
+              content = htmlbase(confirmacion, usuario1, boton)
               //-- Mostrar los datos en la consola del servidor
               console.log("Datos recibidos: " + data)
 
@@ -135,31 +143,89 @@ function peticion(req, res) {
 
            req.on('end', ()=> {
              //-- Generar el mensaje de respuesta
-             res.setHeader('Content-Type', 'text/html')
-             res.write(content);
-             res.end();
+             constructor(req, res, content, "text/html")
            })
            return
         }
+        content = fs.readFileSync(filename, "utf-8")
+        constructor(req, res, content, mime)
       break;
+
+    // Carrito de la compro
+    case '/shoppingcart':
+      // Sacamos los datos de los productos que van a comprarse
+      if (cookie.includes("shoppingcart=")) {
+        let products = recortar(cookie, "=")
+        // Número de productos en el carrito.
+        let numprod = repeticion(cookie, "&")
+
+        var cosas = []
+        for (var i = 0; i < numprod; i++) {
+            find = products.indexOf("&")
+            value = products.slice(0, find)
+            products = products.slice(find+1,)
+            cosas.push(value)
+
+        }
+          cosas.push(products)
+          tipoconf = "<h2>Carrito de la compra</h2>"
+
+          let little = 0;
+          let medium = 0;
+          let big = 0;
+          let price = 0;
+
+          // RECORRER EL ARRAY CON LOS PRODUCTOS Y FORMAR EL html
+          // CON PRODUCTOS + PRECIO TOTAL
+          for (var i = 0; i < cosas.length; i++) {
+            console.log(cosas[i])
+            if (cosas[i] == "Little") {
+              little = little + 1
+              price = price + 14.99
+            } else if (cosas[i] == "Medium") {
+              medium = medium + 1
+              price = price + 29.99
+            } else if (cosas[i] == "Big") {
+              big = big + 1
+              price = price + 34.99
+            }
+          }
+
+          cantidad = "<br><lu>Little Mystery Box: " + little + "</lu>"
+          cantidad += "<br><lu>Medium Mystery Box: " + medium + "</lu>"
+          cantidad += "<br><lu>Big Mystery Box: " + big + "</lu>"
+          cantidad += "<br><br><p>Total compra:       " + price + " $ </p>"
+          boton =  '<a href="/compra" class="button">Confirmar compra</a>'
+          boton += '<a href="/" class="button">Seguir comprando</a>'
+
+      } else {
+        tipoconf = tipoconf = "<h2>Carrito de la compra</h2>"
+        cantidad = "<p>¡No hay nada en tu carrito de la compra!</p>"
+        boton =  '<a href="/" class="button">Seguir comprando</a>'
+      }
+      mime = "text/html"
+      data = htmlbase(tipoconf, cantidad, boton)
+      constructor(req, res, data, mime)
+
+      break;
+
+      // AÑADIR OTRO CASE PARA CONFIRMACIÓN DE COMPRA
+    
 
     //-- El resto de recursos, si no existe la respuesta está contemplada más abajo
     default:
+      // Si añadimos un producto al carrito, añado ese producto a la cookie del carrito
       if (req.method === 'POST') {
           // Handle post info...
           req.on('data', chunk => {
               //-- Leer los datos (convertir el buffer a cadena)
               data = chunk.toString();
-
               //-- Comprobamos que esté la cookie de usuario.
               if (cookie.includes("user=")) {
                 // Buscamos que producto han pedido
                 let order = recortar(data, "=")
-                content = htmlbase("blabla", "___")
-                console.log(filename)
-                console.log("EL FICHERO ES")
 
-                //  Vemos si existe cookie para el carrito
+                //  Vemos si existe cookie para el carrito. Añadimos o creamos
                 if (cookie.includes("shoppingcart=")) {
                   let old_cookie = recortar(cookie, "=")
                   let new_cookie = old_cookie + "&" + order
@@ -177,35 +243,31 @@ function peticion(req, res) {
               }
               // Una vez metido el producto en el carrito, nos quedamos en la misma página
               content = fs.readFileSync(filename, "utf-8")
-              res.statusCode = 200;
+              //res.statusCode = 200;
          });
 
          req.on('end', ()=> {
            //-- Generar el mensaje de respuesta
-           res.setHeader('Content-Type', mime)
-           res.write(content);
-           res.end();
+           constructor(req, res, content, mime)
          })
          return
       }
-      break
 
-      //content = "Error";
-      //res.statusCode = 404;
+      // Montando el mensaje de respuesta para el resto de peticiones
+      fs.readFile(filename, function(err, data) {
+          if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+          }
+          //-- Generar el mensaje de respuesta
+          res.writeHead(200, {'Content-Type': mime});
+          res.write(data);
+          res.end();
+        });
+
+      break
   }
 
-  // Montando el mensaje de respuesta dependiendo del mime
-  fs.readFile(filename, function(err, data) {
-      if (err) {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        return res.end("404 Not Found");
-      }
-
-      //-- Generar el mensaje de respuesta
-      res.writeHead(200, {'Content-Type': mime});
-      res.write(data);
-      res.end();
-    });
 }
 
 //-- Inicializar el servidor
